f053d64dd42766ecb23e5ee8c0250919fb3ea9d7
Brysma <46787593+Brysma@users.noreply.github.com>
2022-03-30 12:56:24 -0700

    Add files via upload
    Two possible csv write scripts for the php element. Need to pull HTML elements into the PHP script.

---
+++ htmlformtocsv2.php
@@ -0,0 +1,18 @@
+//IFT402 File to write CSV from User Questionnaire form submission Option 2
+// Bryce Ungersma
+<?php
+
+$list = array (
+    array('aaa', 'bbb', 'ccc', 'dddd'),
+    array('123', '456', '789'),
+    array('"aaa"', '"bbb"')
+);
+
+$fp = fopen('file.csv', 'w');
+
+foreach ($list as $fields) {
+    fputcsv($fp, $fields);
+}
+
+fclose($fp);
+?>
